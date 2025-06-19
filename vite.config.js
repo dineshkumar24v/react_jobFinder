4e155bbeb4ba4836da9916e798d4975aca850dab
dineshkumar24v <dineshkumar24v@gmail.com>
2025-06-20 00:27:40 +0530

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react_cards_task/', 
+  base: '/react_jobFinder/', 
   plugins: [react()],
 })
